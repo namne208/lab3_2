dca5410821b24c375b5db29bace623b999ac7fa7
Your Name <you@example.com>
2022-06-03 11:10:15 +0700

    Add project files.

---
+++ lab3_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab3_2.Startup))]
+namespace lab3_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
