0f587aef51f68bf08a17bf0bf29b1c2e80716513
Your Name <you@example.com>
2022-06-09 16:44:54 +0700

    Update

--- lab3_2/Controllers/CoursesController.cs
+++ lab3_2/Controllers/CoursesController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using lab3_2.Models;
+using lab3_2.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,43 @@ namespace lab3_2.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         // GET: Courses
+        [Authorize]
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+               LecturerId =  User.Identity.GetUserId(),
+               DateTime = viewModel.GetDataTime(),
+               CategoryId = viewModel.Category,
+               Place = viewModel.Place
+            };
+            _dbContext.Course.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
