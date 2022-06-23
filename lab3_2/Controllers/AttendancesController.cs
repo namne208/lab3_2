80dbfa8ee92f8d4cb74e7585c4344fd5d5f247d8
Your Name <you@example.com>
2022-06-24 02:04:45 +0700

    update 24/6

---
+++ lab3_2/Controllers/AttendancesController.cs
@@ -0,0 +1,57 @@
+﻿using lab3_2.DTOs;
+using lab3_2.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace lab3_2.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var userId = User.Identity.GetUserId();
+            if(_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == courseId))
+            {
+                return BadRequest("The Attendance  alreadly exists!");
+            }
+            var attendance = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeId = userId
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+            {
+                return BadRequest("The Attendance  alreadly exists!");
+            }
+            var attendance = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
